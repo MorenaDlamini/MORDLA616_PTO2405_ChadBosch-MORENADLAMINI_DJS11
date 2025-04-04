d9b3c5c3446f9ee119d1225492aff437de016613
MorenaDlamini <dlaminimorena@gmail.com>
2025-04-04 20:16:20 +0200

    Main Entry File

--- codecast/src/main.tsx
+++ codecast/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// src/main.tsx
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+/**
+ * Main entry point of the React application.
+ * - Uses React 18's `createRoot` API for concurrent rendering.
+ * - Wraps the app in `StrictMode` to surface potential issues.
+ */
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
